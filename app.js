97af44b97b6437d0e8386548eb7e1ace0ff1dd60
Zelic Juarez <zelic.juamtz@gmail.com>
2019-05-23 17:45:01 -0500

    Reading input data and Creating an Initialization Function

--- app.js
+++ app.js
@@ -3,25 +3,56 @@ var budgetController = (function() {
 })();
 
 var UIController = (function() {
-  //some code
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+  var setUpEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
   var ctrlAddItem = function() {
     // 1. get field input data
+    var input = UICtrl.getInput();
     // 2. add item to the budget controller
     // 3. add new item to UI
     // 4. calculate budget
     // 5. display the budget on the UI
-    console.log("it works");
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
-
-  document.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      console.log("The app has started");
+      setUpEventListeners();
     }
-  });
+  };
 })(budgetController, UIController);
+
+controller.init();
